54ad2c1e7555970eb4e3491b19cc16db9c98a5c7
pongsil <hymn@192.168.1.102>
2016-11-27 15:20:12 +0700

    first

---
+++ TableHorizontalScrollView/HorizontalCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  TableHorizontalScrollView
+//
+//  Created by pongsil vachirajongkol on 11/27/2559 BE.
+//  Copyright © 2559 pongsil vachirajongkol. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TableHorizontalScrollView/TableHorizontalViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableHorizontalViewCell.swift
+//  TableHorizontalScrollView
+//
+//  Created by pongsil vachirajongkol on 11/27/2559 BE.
+//  Copyright © 2559 pongsil vachirajongkol. All rights reserved.
+//
+
+import UIKit
+
+class TableHorizontalViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
